39982090d73d4ee53d285abf9f4d71354dc260ce
Daniel <daniel.pearce.exe@gmail.com>
2026-04-20 21:22:32 -0400

    commit 20-04 21:23

--- src/main/java/com/hospital_vm_cl/hospital_vm/Controller/PacienteController.java
+++ src/main/java/com/hospital_vm_cl/hospital_vm/Controller/PacienteController.java
@@ -3,8 +3,11 @@ package com.hospital_vm_cl.hospital_vm.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +32,14 @@ public class PacienteController {
     
         }
         return ResponseEntity.ok(pacientes);
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> guardar(@RequestBody Paciente paciente){
+        Paciente nuevoPaciente= pacienteService.save(paciente);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoPaciente);
 
     }
 }
